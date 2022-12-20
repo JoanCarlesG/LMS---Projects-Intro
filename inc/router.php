a6069cc11a535fb269cbba5dd2beae9b3520fe9b
IgnReLoaD <ortiz.ignasi@gmail.com>
2022-12-20 23:51:51 +0100

    LoginView and button to redirect to studentslist route

--- inc/router.php
+++ inc/router.php
@@ -34,31 +34,31 @@ function loadContent($id_page) {
     switch ($id_page) {
         // dos casos para ir a LOGIN (comprobar si usr/pwd existe en tabla Teachers)
         case "":
-            include '../app/views/login.php';
+            include('app/views/login.php');
             break;
         case "index":
-            include '../app/views/login.php';
+            include('app/views/login.php');
             break;
 
         // un caso para ir a LISTA-ESTUDIANTES (dentro tendrá una sola function searchAll() { return array de N registros de tabla Students}  )
         //    OJO aquí llamará a StudentsController que de momento solo un ECHO (en vez del search) y un "Location reload:StudentsView.php" <-- un html de grid students
         case "studentslist":
-            include '../app/controllers/StudentsController.php';
+            include('app/controllers/StudentsController.php');
             break;                            
 
         // un caso para ir a SINGLE-ESTUDIANTE (dentro tendrá 1 sola function searchById() { return array con una sola posición llenada con 1 registro}  )
         case "studentdetails":
-            include '../app/controllers/StudentController.php';
+            include('app/controllers/StudentController.php');
             break;                            
 
         // un caso para ir a LISTA-EJERCICIOS (dentro tendrá 1 sola function searchAll() { return array de N registros Students_Make_Exercises filtrado por id_student AND id_exercise}  )
         case "exerciseslist":
-            include '../app/controllers/ExercisesController.php';
+            include('app/controllers/ExercisesController.php');
             break;            
             
         // mensaje página no encontrada
         default:
-            include 'inc/error404.php';
+            include('inc/error404.php');
             break;
     }
 }
