a5ce57ca1ee02d6636bd05b23f025f12d6a2d2f2
IgnReLoaD <ortiz.ignasi@gmail.com>
2022-12-20 23:32:54 +0100

    routes - first focus on the topic

---
+++ app/controllers/StudentsController.php
@@ -0,0 +1,22 @@
+<?php
+
+    echo "<br><br> <b>Estamos en StudentsController.php </b> ... y aquí haremos: <br>";
+    echo "1. buscaremos Datos Students (usando métodos de StudentsModel.php) <br>";
+    echo "2. pintaremos Grid Students (redireccionando al StudentsView.php) <br><br>";
+
+    // ANALISIS - PSEUDO-CODIGO:
+
+    // 1. Buscaremos Datos:
+
+    // include "../models/StudentModel.php";
+    // $arrStudents --> será un array de objetos en donde cada objeto será un estudiante con campos definidos en el Model
+    // por tanto: 
+    // 1.1. crearemos $objStudent = new Student() ... y los añadiremos al $arrStudents haciendo Push
+    // 1.2. o bien ejecutamos el método del Model que trae los datos ($query = SELECT * FROM Students WHERE id_teacher='$id_teacher')
+    //      ... que con mysqli($cnn,$query) nos devuelve ya el array de objetos 
+    // return de $arrStudents
+
+    // 2. Pintaremos Grid:
+    header("Location: ../views/StudentsView.php");    /* Redirect browser */
+
+?>
--- inc/router.php
+++ inc/router.php
@@ -6,15 +6,57 @@
  * and open the template in the editor.
  */
 
+
+// DEFINICION DE RUTAS:
+
+// ejemplo con Array Asociativo (recomendado pero más laborioso, habría que implementar función de ruteo)
+$routes = array(
+    // 0. Testejar
+	'/test' => 'test#index',
+    // 1. Landing-IniSessió (rutes per taula TEACHER):
+    '/'         => 'teacher#index',
+    '/index'    => 'teacher#index',    
+	// 2. rutes per taula STUDENTS
+    '/studentslist' => 'student#searchAll',
+    '/studentdetails' => 'student#searchById',  // 'StudentController.php?id=3'... rep per GET id i executa dins el Controller la function searchById($_GET[id]) {return $arrData = ...model.php "SELECT...WHERE..."}
+    // 3. rutes per taula EXERCISES
+    '/exerciseslist' => 'exercises#searchAll'
+);
+
+// ATENCION: esta estructura no tiene array routes, es simple, por tanto NO podemos indicar la función concreta 
+//           a ejecutar a dentro del xxxController.php
+//           ... por tanto, solución rápida: poner varios xxxController con una sola función correspondiente dentro.
 function loadContent($id_page) {
     if((!isset($id_page)) || $id_page=="") {
         $id_page="";
     }
     
     switch ($id_page) {
+        // dos casos para ir a LOGIN (comprobar si usr/pwd existe en tabla Teachers)
         case "":
-            include 'inc/login.php';
-            break;      
+            include '../app/views/login.php';
+            break;
+        case "index":
+            include '../app/views/login.php';
+            break;
+
+        // un caso para ir a LISTA-ESTUDIANTES (dentro tendrá una sola function searchAll() { return array de N registros de tabla Students}  )
+        //    OJO aquí llamará a StudentsController que de momento solo un ECHO (en vez del search) y un "Location reload:StudentsView.php" <-- un html de grid students
+        case "studentslist":
+            include '../app/controllers/StudentsController.php';
+            break;                            
+
+        // un caso para ir a SINGLE-ESTUDIANTE (dentro tendrá 1 sola function searchById() { return array con una sola posición llenada con 1 registro}  )
+        case "studentdetails":
+            include '../app/controllers/StudentController.php';
+            break;                            
+
+        // un caso para ir a LISTA-EJERCICIOS (dentro tendrá 1 sola function searchAll() { return array de N registros Students_Make_Exercises filtrado por id_student AND id_exercise}  )
+        case "exerciseslist":
+            include '../app/controllers/ExercisesController.php';
+            break;            
+            
+        // mensaje página no encontrada
         default:
             include 'inc/error404.php';
             break;
