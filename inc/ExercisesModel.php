2a5f005885d2b762a529a18277170d217a36e196
JoanCarlesG <juancarlos.godinezm@gmail.com>
2022-12-21 13:15:34 +0100

    Initial commit Stats

---
+++ inc/ExercisesModel.php
@@ -0,0 +1,23 @@
+<?php 
+class Exercise{
+    //Attributes
+    public $title = "";
+    public $description = "";
+    public $grade = 0.0;
+    public $deliveryDate = date("Y/m/d");
+    
+    
+
+
+    //Getters
+
+
+    //Setters
+
+
+    //Methods
+
+
+}
+
+?>
