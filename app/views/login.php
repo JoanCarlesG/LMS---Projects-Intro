3dc1384edf7a14d26e8479f88c48e582850c7674
GabrielaMaureira <gama@Gabriela-Maureira.local>
2022-12-22 09:27:19 +0100

    Branch and login view done

--- app/views/login.php
+++ app/views/login.php
@@ -1,23 +1,33 @@
-<div class="flex h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat" style="background-image:url('https://images.unsplash.com/photo-1499123785106-343e69e68db1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80')">
+<?php
+include('loginStyles.css');
+?>
+<div class="flex h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat" style="background-color: #d5b59c">
   <div class="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
     <div class="text-white">
+      <!--  Tittle area --> 
       <div class="mb-8 flex flex-col items-center">
-        <img src="img/libro_abierto.png" width="150" alt="" srcset="" />
-        <h1 class="mb-2 text-2xl">Welcome to LMS Learning Management System</h1>
-        <span class="text-gray-300">Enter Login Details</span>
+        
+        <h1 class="text">Welcome to LMS</h1>
+        
       </div>
+      <!--  Login form area -->
       <form action= "loadContent(teachercheck)">
-        <div class="mb-4 text-lg">
-          <input class="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md" type="text" name="name" placeholder="id@email.com" />
+       <!--  Username area -->
+        <div class="mt-16 flex flex-col xl:w-2/6 lg:w-1/2 md:w-1/2 w-full">
+           <label for="username" class="text">Username</label>
+           <input tabindex="0" type="text" id="username" name="username" required class="border border-gray-300 dark:border-gray-700 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-indigo-700 bg-transparent placeholder-gray-500 text-gray-600 dark:text-gray-400" placeholder="mentor" />
         </div>
-
-        <div class="mb-4 text-lg">
-          <input class="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md" type="Password" name="name" placeholder="*********" />
-        </div>
-        <div class="mt-8 flex justify-center text-lg text-black">
-          <button type="submit" class="rounded-3xl bg-yellow-400 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600">Login</button>
+        <!--  Password area -->
+        <div class="mt-16 flex flex-col xl:w-2/6 lg:w-1/2 md:w-1/2 w-full">
+           <label for="username" class="text">Password</label>
+           <input tabindex="0" type="text" id="username" name="username" required class="border border-gray-300 dark:border-gray-700 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-indigo-700 bg-transparent placeholder-gray-500 text-gray-600 dark:text-gray-400" placeholder="******" />
+        </div>     
+        <!--  Button area -->
+        <div class="mt-8 flex justify-center text-lg text-white">
+          <button type="submit" class="btn">Log in</button>
         </div>
       </form>
+      </div>
     </div>
-  </div>
 </div>
+        
